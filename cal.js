6da7a3cb9e92d087f70989d9044ff71e4698cedc
Isha <ishurawat267@gmail.com>
2024-03-12 14:14:55 +0530

    new commit

---
+++ cal.js
@@ -0,0 +1,32 @@
+const input = document.getElementById('content');
+const buttons = document.querySelectorAll('button');
+
+function calculate(expression) {
+    console.log(expression);
+    console.log(typeof(expression));
+    try {
+        return new Function('return ' + expression)();
+    } catch (error) {
+        return 'Malformed Operation';
+    }
+}
+
+
+function operation(buttonValue) {
+    if (buttonValue === 'AC') {
+        input.value = '';
+    } else if (buttonValue === 'DEL') {
+        input.value = input.value.slice(0, -1);
+    } else if (buttonValue === '=') {
+        input.value = calculate(input.value);
+    } else {
+        input.value += buttonValue;
+    }
+}
+
+buttons.forEach(button => {
+    let buttonValue = button.innerText;
+    button.addEventListener('click', function () {
+        operation(buttonValue);
+    });
+});
